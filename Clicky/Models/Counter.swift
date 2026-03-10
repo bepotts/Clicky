6f4dcf6c8bacac6059392c230dbed1146aa22d20
Brandon Potts <ObeliskIsHuge@gmail.com>
2026-03-10 11:39:34 -0400

    added id to the counter class

--- Clicky/Models/Counter.swift
+++ Clicky/Models/Counter.swift
@@ -10,11 +10,14 @@ import SwiftData
 
 @Model
 final class Counter {
+    @Attribute(.unique) var id: UUID
     var count: Int
     var name: String
     var incrementBy: Int
 
-    init(count: Int = 0, name: String = "", incrementBy: Int = 1) {
+    init(id: UUID = UUID(), count: Int = 0, name: String, incrementBy: Int = 1) {
+        precondition(!name.isEmpty, "Counter name must not be empty")
+        self.id = id
         self.count = count
         self.name = name
         self.incrementBy = incrementBy
